16b0341b29a980f89963d376c1a1d52aa4d95aef
hunee <122859262+hunee01@users.noreply.github.com>
2025-10-21 13:34:57 -0500

    Modify set_ps1 to include display character

--- karel/config/set_config.h
+++ karel/config/set_config.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void set_ps1(const char *file , const char *key, const char *value) {
+void set_ps1(const char *file , const char *key, const char *value, const char disp) {
     // RECONSTRUCT NEW JSON STRING
     FILE *conf = fopen(file, "w");
     if (!conf) {
@@ -10,6 +10,6 @@ void set_ps1(const char *file , const char *key, const char *value) {
         return;
     }
     
-    fprintf(conf, "{\n    \"disp\": 0,\n    \"ps1\": \"%s\"\n}\n",value);
+    fprintf(conf, "{\n    \"disp\": %c,\n    \"ps1\": \"%s\"\n}\n",disp,value);
     fclose(conf);
 }
