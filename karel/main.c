a2abce4aa10db6cd18574bf30c79326e9967c352
hunee <122859262+hunee01@users.noreply.github.com>
2025-10-21 15:44:55 -0500

    Finally updated version number within code...
    Updated from 1.1-beta to 1.2.1-beta (A little late)

--- karel/main.c
+++ karel/main.c
@@ -23,7 +23,7 @@ int main() {
     char cwd[1024];
     int pos;
     char hostname[1024];
-    char *VERSION = "1.1-beta";
+    char *VERSION = "1.2.1-beta";
     gethostname(hostname, sizeof(hostname));
     char *USER = getenv("USER");
     int mode = 0777;
