c283efcbb0e3113aca0f00c9744c2613790d3a78
hunee <122859262+hunee01@users.noreply.github.com>
2025-10-22 12:15:10 -0500

    Refactor set_config.h for new command handling
    Refactor set_ps1 function and add configuration handling

--- karel/config/set_config.h
+++ karel/config/set_config.h
@@ -2,7 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-void set_ps1(const char *file , const char *key, const char *value, const char disp) {
+char *file = "kcsh.json";
+
+typedef struct {
+    char *configChar;
+    int disp;
+    char *ps1;
+} Initialisation;
+
+typedef struct {
+    long fileSize;
+    char *buffer;
+    size_t bytesRead;
+} ConfigInfo;
+
+ConfigInfo main_conf() {
+    ConfigInfo result;
+    
+    FILE *conf = fopen(file,"r");
+    fseek(conf, 0, SEEK_END);
+    result.fileSize = ftell(conf);
+    fseek(conf, 0, SEEK_SET);
+    result.buffer = malloc(result.fileSize + 1);
+    result.bytesRead = fread(result.buffer, 1, result.fileSize, conf);
+    result.buffer[result.bytesRead] = '\0';
+    fclose(conf);
+    
+    return result;
+}
+
+Initialisation init_conf(const char *buffer) {
+    Initialisation result;
+    
+    // SETTING CONFIGS
+    result.ps1 = strstr(buffer, "\"ps1\"") + 8;
+    for (int i = 0; i < sizeof(result.ps1); i++) {
+        if (result.ps1[i] == '"') {
+            result.ps1[i] = '\0';
+        }
+    }
+    
+    result.configChar = strstr(buffer, "\"disp\"") + 8;
+    result.disp = *result.configChar - '0';
+    
+    return result;
+}
+
+void set_ps1(const char *key, const char *value, const char disp) {
     // RECONSTRUCT NEW JSON STRING
     FILE *conf = fopen(file, "w");
     if (!conf) {
@@ -12,4 +58,20 @@ void set_ps1(const char *file , const char *key, const char *value, const char d
     
     fprintf(conf, "{\n    \"disp\": %d,\n    \"ps1\": \"%s\"\n}\n",disp,value);
     fclose(conf);
+}
+
+void set_disp(int setVal,char *buffer,const char *configChar,const long fileSize,size_t bytesRead) {
+    FILE *conf = fopen(file,"r+");
+    bytesRead = fread(buffer, 1, fileSize, conf);
+    buffer[bytesRead] = '\0';
+    
+    configChar = strstr(buffer, "\"disp\"") + 8;
+    char *configPtr = strchr(buffer,configChar[0]);
+    fseek(conf,configPtr - buffer,SEEK_SET);
+    if (fputc(setVal,conf) != EOF) {
+        fclose(conf);
+    }
+    else {
+        printf("conf: failed to set configuration: %c",setVal);
+    }
 }
