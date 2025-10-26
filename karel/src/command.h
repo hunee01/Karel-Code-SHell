943605b79c696a428ee7c0b530e765d3eae6cc49
heirloom hunee <122859262+hunee01@users.noreply.github.com>
2025-10-26 18:07:47 -0500

    Update command.h for 'rd'

--- karel/src/command.h
+++ karel/src/command.h
@@ -6,7 +6,7 @@ void help(void);
 void echo(const char *args);
 void pwd(const char *cwd);
 void cd(const char *dir);
-void rm(const char *file);
+void rm(const char *delMode,const char *file);
 void c_mkdir(const char *dir);
 void touch(const char *file);
 void rd(const char *file);
